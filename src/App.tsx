1a13d35e5acf8602f4629f8c8305041b6336d0b4
paulotruly <paulomsmsms@gmail.com>
2025-11-13 11:39:38 -0300

    comecando

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
+      <Header></Header>
       <Home></Home>
     </>
   )
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+
+    return (
+        <>
+            <div className="flex flex-col">
+                <div className="flex flex-row h-14 bg-black"> </div>
+                <div className="flex flex-row justify-between items-center h-16 bg-[#F3F2F2]"> Ooi </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
