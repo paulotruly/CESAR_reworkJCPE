7ee4202452f335fad544092fd9bf5052f4639fdc
paulotruly <paulomsmsms@gmail.com>
2025-11-17 13:11:00 -0300

    header

---
+++ src/assets/SearchIcon.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const SearchIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <circle
+      cx="21"
+      cy="21"
+      r="12"
+      stroke="currentColor"
+      strokeWidth="4"
+    />
+    <line
+      x1="30"
+      y1="30"
+      x2="40"
+      y2="40"
+      stroke="currentColor"
+      strokeWidth="4"
+      strokeLinecap="round"
+    />
+  </svg>
+);
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,44 @@
+import { MenuIcon } from "../assets/MenuIcon"
+import { SearchIcon } from "../assets/SearchIcon"
+
 function Header() {
 
     return (
         <>
             <div className="flex flex-col">
-                <div className="flex flex-row h-14 bg-black"> </div>
-                <div className="flex flex-row justify-between items-center h-16 bg-[#F3F2F2]"> Ooi </div>
+
+                <div className="flex flex-row justify-center items-center h-14 bg-black">
+                    <img
+                    className="w-auto h-5"
+                    src="https://vitorbotega.com.br/wp-content/uploads/2022/12/negativo.png"
+                    alt=""
+                    />
+                </div>
+                
+                <div className="flex flex-row items-center h-16 p-2 bg-[#F3F2F2]">
+
+                    <img
+                    className="mr-auto w-auto h-[45px]"
+                    src="https://jc.uol.com.br/img/edicao.png"
+                    alt=""
+                    />
+
+                    <img
+                    className="ml-6 w-auto h-10"
+                    src="https://blogdomagno.com.br/wp-content/uploads/2024/07/1_logo_jc-27530572.png"
+                    alt=""
+                    />
+
+                    <nav className="ml-auto flex flex-row gap-4">
+                        <a href="">
+                            <MenuIcon width={25}></MenuIcon>
+                        </a>
+                        <a href="">
+                            <SearchIcon width={25}></SearchIcon>
+                        </a>
+                    </nav>
+
+                </div>
             </div>
         </>
     )
