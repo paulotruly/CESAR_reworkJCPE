53f74f968a75716944899bd168c33e7b6ee89615
paulotruly <paulomsmsms@gmail.com>
2025-11-19 13:31:04 -0300

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { MenuIcon } from "../assets/MenuIcon"
 import { SearchIcon } from "../assets/SearchIcon"
+import { RightArrow } from "../assets/RightArrow"
 
 function Header() {
 
@@ -28,14 +29,24 @@ function Header() {
                 </nav>
             </div>
 
-            <ul className="group flex flex-row items-center justify-center min-w-screen h-auto text-sm bg-[#F1F0F0]">
+            <div className="group flex flex-row items-center justify-center w-full h-auto text-sm bg-[#F1F0F0]">
                 <a className="text-[#DF1E26] p-2 hover:bg-[#DF1E26] hover:text-white" href=""> Política </a>
                 <a className="text-[#DF1E16] p-2 hover:bg-[#DF1E26] hover:text-white" href=""> Mundo </a>
                 <a className="text-[#DF1E16] p-2 hover:bg-[#DF1E26] hover:text-white" href=""> Economia </a>
                 <a className="text-[#DF1E16] p-2 hover:bg-[#DF1E26] hover:text-white" href=""> Ciência e Tecnologia </a>
 
-                <li> <button className="text-[#DF1E26] text-right p-2 font-semibold hover:bg-[#DF1E26] hover:text-white"> Mais </button> </li>
-            </ul>
+                <button className="text-[#DF1E26] text-right p-2 font-semibold hover:bg-[#DF1E26] hover:text-white"> Mais </button>
+            </div>
+
+            <div className="flex flex-row items-center bg-white w-full h-auto p-2 px-3 text-[13px]">
+                <div className="flex flex-row items-center mr-4 text-[#DF1E26] gap-2">
+                    <p className="font-bold tracking-tighter text-[14px]"> 11:40 - QUI </p>
+                    <RightArrow></RightArrow>
+                </div>    
+                <a className="px-2 text-black" href=""> Esportes </a>
+                <a className="px-2 text-black" href=""> Cultura </a>
+                <a className="px-2 text-black" href=""> Saúde </a>
+            </div>
 
         </div>
     )
