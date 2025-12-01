c54fc820b7e8b0d728c85759900ea460f295586e
Paulo <paulomsmsms@gmail.com>
2025-11-30 22:56:08 -0300

    paginacategorias

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Noticia from './pages/Noticia'
 import Header from './components/Header';
 import Rodape from './components/Rodape';
+import Categoria from './pages/Categoria';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/noticias/:id" element={<Noticia />} />
+            <Route path="/categoria" element={<Categoria />} />
           </Routes>
         <Rodape></Rodape>
     </>
